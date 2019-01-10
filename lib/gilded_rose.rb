ff19fd5c81e0b13999cd402b1feba0302366ca5b
Rupert Adams <RupertNCAdams@aol.com>
2019-01-10 10:23:11 +0000

    began seperating the responsabilities, starting with basic unpdater for items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,6 +4,14 @@ class GildedRose
     @items = items
   end
 
+  def legendary(item)
+  end
+
+  def updater(item)
+    item.quality = 0 if item.quality < 0
+    item.quality = 50 if item.quality > 50
+  end
+
   def update_quality
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
