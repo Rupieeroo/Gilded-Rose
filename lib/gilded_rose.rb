a4e43a8fb580f9e5ea8f4368fb4c08b9b33ef16a
Rupert Adams <RupertNCAdams@aol.com>
2019-01-14 14:27:23 +0000

    added spec code for gilded rose class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,41 +8,45 @@ class GildedRose
   def initialize(items)
     @items = items
   end
-
-  def updater
-    item = Updater.new(@items)
-    item.updater
-  end
-
-  def aged_brie_update
-    item = AgedBrie.new(@items)
-    item.update
-    item.final_check
-  end
-
-  def backstage_pass_update
-    item = BackstagePass.new(@items)
-    item.update
-    item.final_check
-  end
-
-  def normal_update
-    item = Normal.new(@items)
-    item.update
-    item.final_check
-  end
+  #
+  # def updater
+  #   item = Updater.new(@items)
+  #   item.updater
+  # end
+  #
+  # def aged_brie_update
+  #   item = AgedBrie.new(@items)
+  #   item.update
+  #   item.final_check
+  # end
+  #
+  # def backstage_pass_update
+  #   item = BackstagePass.new(@items)
+  #   item.update
+  #   item.final_check
+  # end
+  #
+  # def normal_update
+  #   item = Normal.new(@items)
+  #   item.update
+  #   item.final_check
+  # end
+  #
+  # def update_quality
+  #   @items.each do |item|
+  #     case item.name
+  #       when 'Aged Brie'
+  #         aged_brie_update
+  #       when 'Backstage passes to a TAFKAL80ETC concert'
+  #         backstage_pass_update
+  #       else
+  #         normal_update unless item.name == 'Sulfuras, Hand of Ragnaros'
+  #     end
+  #   end
+  # end
 
   def update_quality
-    @items.each do |item|
-      case item.name
-        when 'Aged Brie'
-          aged_brie_update
-        when 'Backstage passes to a TAFKAL80ETC concert'
-          backstage_pass_update
-        else
-          normal_update unless item.name == 'Sulfuras, Hand of Ragnaros'
-      end
-    end
+    @items.each { |item| item.update }
   end
 end
 
