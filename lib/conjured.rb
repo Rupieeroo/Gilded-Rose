850727757022dbd2cbd9e82a326a4ce00e49d456
Rupert Adams <RupertNCAdams@aol.com>
2019-01-15 11:55:59 +0000

    passed and fixed tests for conjured items

--- lib/conjured.rb
+++ lib/conjured.rb
@@ -2,4 +2,11 @@ require 'item'
 
 class Conjured < Item
 
+  def update
+    @sell_in -= 1
+    @quality -= 2
+    @quality -= 2 if @sell_in <= 0
+    @quality = 0 if @quality < 0
+    @quality = 50 if @quality > 50
+  end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -149,8 +149,8 @@ describe GildedRose do
         expect(item.name).to eq "foo"
       end
 
-      it "lowers the sell_in by two after a day" do
-        item = Conjured.new("item", 2, 0)
+      it "lowers the sell_in by one after a day" do
+        item = Conjured.new("item", 1, 0)
         GildedRose.new([item]).update_quality
         expect(item.sell_in).to eq 0
       end
